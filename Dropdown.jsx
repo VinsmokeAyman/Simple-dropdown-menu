7714367a0bd1b55cd0dfe53778f7bf79abec1274
Laqlii Ayman <97404704+VinsmokeAyman@users.noreply.github.com>
2025-04-02 14:51:25 +0000

    Add files via upload

---
+++ Dropdown.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { animate, motion, useAnimation } from 'framer-motion';
+import Down from '../Icons/icons8-expand-arrow-50.png'
+
+
+function Dropdown({info}) {
+
+    const [OpenDropdown,setOpen] = useState(false)
+    const [Selected,setSelected] = useState('Select an item')
+
+  return (
+    <div className='hover' onClick={() => {setOpen(!OpenDropdown)}} style={{display:'flex',backgroundColor:'#fff',alignItems:'center',justifyContent:'space-between',flexDirection:'row',position:'relative',width:'200px',height:'35px',borderRadius:'7px',border:'2px solid #2a56cc'}}>
+        {OpenDropdown?<motion.div initial={{y:-10,opacity:0}} animate={{y:0,opacity:1}} transition={{duration:0.4}}  style={{display:'flex',flexDirection:'column',backgroundColor:'#fff',position:'absolute',height:'200px',width:'100%',borderRadius:'7px',boxShadow:' -5px 10px 20px 5px #C0C0C0',top:'50px'}}>
+            {info.map((item) => (
+                <div onClick={()=>{setSelected(item)}} className='hover2' style={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'center',width:'100%',height:'40px'}}>
+                    <p className='Title' style={{fontSize:'16px',fontWeight:'600',color:'black',margin:'0 0 0 0px'}}> {item} </p>
+                </div>
+            ))}
+        </motion.div>:''}
+        <p className='Title' style={{fontSize:'16px',fontWeight:'600',color:'black',margin:'0 0 0 7px'}}>{Selected}</p>
+        {!OpenDropdown?<img src={Down} style={{height:'20px',width:'20px',margin:'0 7px 0 0'}}/>:<img src={Down} style={{height:'20px',rotate:'180deg',width:'20px',margin:'0 7px 0 0'}}/>}
+    </div>
+  )
+}
+
+export default Dropdown
